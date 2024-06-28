51461a8c760d3e26219c8889d22388b74924edb5
Sir Code <taylorschneider07@yahoo.com>
2024-06-28 14:25:43 -0400

    Rewriting

--- README.md
+++
@@ -1,5 +0,0 @@
-# Quilez Laser
-
-A mod for teardown that adds a handheld version of the laser from quilez security
-
-[View on Steam](https://steamcommunity.com/sharedfiles/filedetails/?id=2697004953)
--- main.lua
+++ main.lua
@@ -1,2 +1,19 @@
-#include "umf_complete_c.lua"
-#include "quilezlaser.lua"
+--This script will run on all levels when mod is active.
+--Modding documentation: http://teardowngame.com/modding
+--API reference: http://teardowngame.com/modding/api.html
+
+function init()
+end
+
+function tick(dt)
+end
+
+
+function update(dt)
+end
+
+
+function draw(dt)
+end
+
+
